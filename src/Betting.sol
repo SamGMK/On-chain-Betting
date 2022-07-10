40a57569f13b07b3500ba0863ef61df822f6c9ae
Samater Phi <Samaterphi77@gmail.com>
2022-07-10 02:50:35 -0700

    initial commit

--- src/Betting.sol
+++ src/Betting.sol
@@ -1,4 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Contract {}
+contract Betting {
+    /// @title an on-chain sports betting contract with Liquidity provision mechanism
+    /// @author @Sam GMK
+    /// @notice  contract consists of two parts: 1) Liquidity provision. 2) Betting against sporting events.
+    /// Users can bet on sporting events and if they win they get paid their capital plus winnings. If 
+    /// they lose the house(smart contract) keeps their capital. The capital is then distributed  to 
+    /// Liquidity Providers(LPs) relative to their share of LP tokens.
+    /// @dev Owner of the contract has the power to activate fees.
+}
